32d18a6bd87c196aeb88ef5f5d9fe01585b75ba4
JensIssa <71692396+JensIssa@users.noreply.github.com>
2022-01-05 21:49:12 +0100

    Oprettede objekterne Category og Movie

--- src/be/Movie.java
+++ src/be/Movie.java
@@ -1,4 +1,60 @@
 package be;
 
+import java.util.Date;
+
 public class Movie {
+
+    private int id;
+    private String name;
+    private float rating;
+    private String fileLink;
+    private Date lastView;
+
+    public Movie(int id, String name, float rating, String fileLink, Date lastView) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+        this.fileLink = fileLink;
+        this.lastView = lastView;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getFileLink() {
+        return fileLink;
+    }
+
+    public void setFileLink(String fileLink) {
+        this.fileLink = fileLink;
+    }
+
+    public Date getLastView() {
+        return lastView;
+    }
+
+    public void setLastView(Date lastView) {
+        this.lastView = lastView;
+    }
 }
