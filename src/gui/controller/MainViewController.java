ee9f4a5d095ff1175520195b55cd046f503d28e2
AndreasBerthelsen <andr59j9@easv365.dk>
2022-01-12 11:24:21 +0100

    Play movie fixed

--- src/gui/controller/MainViewController.java
+++ src/gui/controller/MainViewController.java
@@ -14,6 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -193,10 +194,16 @@ public class MainViewController implements Initializable {
     }
 
     public void handleButtonPlay(ActionEvent actionEvent) throws IOException {
-        Movie movie = allMoviesTable.getSelectionModel().getSelectedItem();
-        java.sql.Timestamp lastview = new java.sql.Timestamp(System.currentTimeMillis());
-        movie.setLastView(lastview);
-        Desktop.getDesktop().open(new File(moviePath + movie.getFileLink()));
+        if (allMoviesTable.getSelectionModel().getSelectedItem() != null){
+            Movie movie = allMoviesTable.getSelectionModel().getSelectedItem();
+            Desktop.getDesktop().open(new File(moviePath + movie.getFileLink()));
+        }
+        else if (moviesTable.getSelectionModel().getSelectedItem() != null){
+            Movie movie2 = moviesTable.getSelectionModel().getSelectedItem();
+            Desktop.getDesktop().open(new File(moviePath + movie2.getFileLink()));
+        }
+        else
+            error("Please select a movie");
 
     }
 
@@ -211,4 +218,9 @@ public class MainViewController implements Initializable {
         Movie selectedMovie = moviesTable.getSelectionModel().getSelectedItem();
         categoryModel.deleteMovieFromCategory(selectedCategory, selectedMovie);
     }
+
+    public void handleMovieTableClicked(MouseEvent mouseEvent) {
+        allMoviesTable.getSelectionModel().clearSelection();
+    }
+    
 }
