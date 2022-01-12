56c1221eebdb002246360cf473de30020f4e1618
Jens Bassem Hanna Issa <jens7391@easv365.dk>
2022-01-12 12:18:09 +0100

    MediaPlayer made to be progressed

---
+++ src/gui/Model/MoviePlayModel.java
@@ -0,0 +1,14 @@
+package gui.Model;
+
+import be.Movie;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+public class MoviePlayModel {
+
+    private MediaPlayer mediaPlayer;
+
+    public MoviePlayModel(Movie movie){
+    }
+
+}
