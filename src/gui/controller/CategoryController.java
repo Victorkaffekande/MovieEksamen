53ad13e804c34103a5842cf957ec81d70e4d85fa
Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>
2022-01-19 10:38:33 +0100

    categoryController kommentar

--- src/gui/controller/CategoryController.java
+++ src/gui/controller/CategoryController.java
@@ -6,7 +6,12 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public abstract class CategoryController {
-
+    /**
+     * finder texten i det givne tekstfelt
+     * @param nameInput navnet på det ønskede tekstfelt
+     * @return Stringen fra tekstfeltet trimmet. hvis der kun er
+     * mellemrum i tekstfeltet, return null
+     */
     public String getName(TextField nameInput){
         if (!nameInput.getText().trim().isEmpty()){
             return nameInput.getText().trim();
@@ -14,15 +19,28 @@ public abstract class CategoryController {
         return null;
     }
 
+    /**
+     * lukker vinduet
+     * @param anyButton en knap der befinder sig i det vindue der skal lukkes
+     */
     public void closeWindow(Button anyButton){
         Stage stage = (Stage) anyButton.getScene().getWindow();
         stage.close();
     }
+
+    /**
+     * Lukker vinduet
+     * @param anyTextField et tekstefelt der befinder sig i det vindue der skal lukkes
+     */
     public void closeWindow(TextField anyTextField){
         Stage stage = (Stage) anyTextField.getScene().getWindow();
         stage.close();
     }
 
+    /**
+     * åbner et error vindue
+     * @param errorString Error teksten der skal vises i alert boksen
+     */
     public void error(String errorString){
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Warning");
